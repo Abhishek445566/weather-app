eb11c3d971426f38560801e956f1aef5a89ec879
abhishek-goyal <45648611+Abhishek445566@users.noreply.github.com>
2020-12-29 15:16:24 +0530

    Add files via upload

---
+++ src/app_component/heading.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./heading.style.css";
+
+const Heading = props =>{
+		return(
+		<div className="brightness">
+		<h1>Lets Check Todays weather of any location !
+		</h1>
+		</div>
+	);	
+};
+
+export default Heading;
